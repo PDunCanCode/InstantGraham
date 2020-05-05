e74c7bd82920a4b4f50ab1aac08f8e05b603a926
Paul D. Duncan <chesiredarling@gmail.com>
2020-05-05 14:36:28 -0700

    Begin/ Prep for now deplot

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -3,12 +3,18 @@ import { useEditProfilePageStyles } from '../styles';
 import Layout from '../components/shared/Layout';
 import {
   IconButton,
+  Button,
   Hidden,
   Drawer,
+  List,
   ListItem,
   ListItemText,
+  Typography,
+  TextField,
 } from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
+import { defaultCurrentUser } from '../data';
+import ProfilePicture from '../components/shared/ProfilePicture';
 
 function EditProfilePage({ history }) {
   const classes = useEditProfilePageStyles();
@@ -114,6 +120,95 @@ function EditProfilePage({ history }) {
     </Layout>
   );
 }
-function EditUserInfo() {}
+function EditUserInfo({ user }) {
+  const classes = useEditProfilePageStyles();
+
+  return (
+    <section className={classes.container}>
+      <div className={classes.pictureSectionItem}>
+        <ProfilePicture size={38} user={user} />
+        <div className={classes.justifySelfStart}>
+          <Typography className={classes.typograhy}>{user.username}</Typography>
+          <Typography
+            color='primary'
+            variant='body2'
+            className={classes.typographyChangePic}
+          >
+            Change Profile Photo
+          </Typography>
+        </div>
+      </div>
+      <form className={classes.form}>
+        <SectionItem text='Name' formItem={user.name} />
+        <SectionItem text='Username' formItem={user.username} />
+        <SectionItem text='Website' formItem={user.website} />
+        <div className={classes.SectionItem}>
+          <aside>
+            <Typography className={classes.bio}>Bio</Typography>
+          </aside>
+          <TextField
+            variant='outlined'
+            multiline
+            rowsMax={3}
+            rows={3}
+            fullWidth
+            value={user.bio}
+          />
+        </div>
+        <div className={classes.sectionItem}>
+          <div />
+          <Typography
+            color='textSecondary'
+            className={classes.justifySelfStart}
+          >
+            {' '}
+            Personal Information
+          </Typography>
+        </div>
+        <SectionItem text='Email' formItem={user.email} type='email' />
+        <SectionItem text='Phone Number' formItem={user.phone_number} />
+        <div className={classes.sectionItem}>
+          <div />
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            className={classes.justifySelfStart}
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+    </section>
+  );
+}
+function SectionItem({ type = 'text', text, formItem }) {
+  const classes = useEditProfilePageStyles();
+
+  return (
+    <div className={classes.sectionItemWrapper}>
+      <aside>
+        <Hidden xsDown>
+          <Typography className={classes.typography} align='right'>
+            {text}
+          </Typography>
+        </Hidden>
+        <Hidden smUp>
+          <Typography className={classes.typography}>{text}</Typography>
+        </Hidden>
+      </aside>
+      <TextField
+        variant='outlined'
+        fullWidth
+        value={formItem}
+        type={type}
+        className={classes.textField}
+        inputProps={{
+          className: classes.textFieldInput,
+        }}
+      />
+    </div>
+  );
+}
 
 export default EditProfilePage;
